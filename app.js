c1805b3780ba205d200267a40e03893b1da9cc74
root <root@VarshithaReddy.localdomain>
2022-12-24 17:25:02 +0530

    Added EJS

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", function (request, response) {
-  response.send("Hello World");
+app.get("/", async (request, response) => {
+  try {
+    const allTodos = await Todo.getTodos();
+    if (request.accepts("html")) {
+      response.render("index", {
+        title: "Todo_App",
+        allTodos,
+      });
+    } else {
+      response.json({
+        allTodos,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
 });
 
 app.get("/todos", async function (_request, response) {
@@ -52,8 +70,13 @@ app.put("/todos/:id/markAsCompleted", async function (request, response) {
 
 app.delete("/todos/:id", async function (request, response) {
   console.log("We have to delete a Todo with ID: ", request.params.id);
-  const deleteRes = await Todo.destroy({ where: { id: request.params.id } });
-  response.send(deleteRes == 1);
+  try {
+    const deleteRes = await Todo.destroy({ where: { id: request.params.id } });
+    response.send(deleteRes == 1);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
 });
 
 module.exports = app;
